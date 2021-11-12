b2da1f3056aa6cdb5a0d346fc9ab69354bc3891c
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2021-11-12 17:04:01 +0100

    Gonna change everything

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
-let activeScene = 0
+// let activeScene = 0
+
 // const inventory = [2]
 // let inventory = 1
 // const activeScene  = []
-
-
 // let displayScene1 = function () {
 //     activeScene= 0 
 // }
 
-let displayScene2 = function () {
-    activeScene= 1
+let displayEnterScene = function () {
     const pageInfo = document.querySelector('#pageInfo')
     pageInfo.classList.toggle("active")
     const pageStart = document.querySelector('#pageStart')
     pageStart.classList.toggle("active")    
 }
 
-let displayScene3 = function () {
-    activeScene= 2
+let chooseHeroScene = function () {
     const pageHero = document.querySelector('#pageHero')
     pageHero.classList.toggle("active")
     const pageInfo = document.querySelector('#pageInfo')
-    pageInfo.classList.toggle("active")
-    
+    pageInfo.classList.toggle("active")    
 }
 
-nextScene = function () {
-    if (activeScene === 0) {
-        displayScene2()
-    }
-    
-    else if (activeScene === 1) {
-        displayScene3()
-    }
-    else if (activeScene === 2) {
-        // displayScene4()
-    }
+let displayDeclineScene = function(){
+        const pageDecline = document.querySelector('#decline')
+        pageDecline.classList.toggle("active")
 }
+
+
+// declineScene = function(){
+//     activescene= 5
+//     nextScene()
+// }
+// nextScene = function () {
+//     if (activeScene === 0) {
+//         displayEnterScene()
+//     }
+    
+//     // else if (activeScene === 1) {
+//     //     displayScene3()
+//     // }
+//     // else if (activeScene === 2) {
+//     //     // displayScene4()
+//     // }
+//     // else{
+//     // displayDeclineScene()
+//     // }
+    
+// }
 
 
 
