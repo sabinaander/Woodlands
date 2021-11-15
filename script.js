1506a3b7a4c12597fc5984511d6c67b9f31e3304
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2021-11-15 16:51:00 +0100

    Some more scenes added

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ const pages = [
         </div>
     </div>
     `
+    ,
+    nextScenes: [2, DEAD]
     },
     {// Scene 2 (Chosen "I AM BRAVE" show Hero-info)
         content: /*html*/`
@@ -56,6 +58,8 @@ const pages = [
         <button onclick="getName()">LET'S DO THIS!</button>
     </div>
     `
+,
+nextScenes: [4]
 
     },
     {// Scene 3 (Chosen "NOPE" or dead in fight show Tombstone + Play Again? btn)
@@ -66,6 +70,8 @@ const pages = [
         <button onclick="changeScene(0)">PLAY AGAIN?</button>
     </div>
     `
+    ,
+    nextScenes: [0]
     }
     ,
     {// Scene 4 (Chosen "LETS DO THIS", function to be able to show name)
@@ -82,27 +88,30 @@ const pages = [
             <img onclick="changeScene(5)" class="nextArrow" src="/media/nextarrow.png"> 
             </div>
     </div>
-    `
-        }
+    `} ,
+    nextScenes: [5]
     }
     ,
     {// Scene 5 (Mushroom-scene. Make a cruicial choice!)
         content: /*html*/ `
-    <div class="centerContainer">
-        <div class="box card">
-        <img src="/media/shroom.png" class="pixelImg">
-            <p>You've been wandering through the woods for a long time.<br>
-            Energy is running low, tummy's rumbling...<br>
-            You come across a strange looking mushroom in the lands.<br>
-            It looks delicious, but is it safe to eat something like this?
-        </p>
-        </div>
-        <div class="dualButtons">
-            <button onclick="changeScene(6)">TAKE THE<br> CHANCE!</button>
-            <button class ="redText" onclick="changeScene(7)">STRANGER<br>DANGER!</button>
-        </div>
-    </div>
-    `
+            <div class="centerContainer">
+                <div class="box card">
+                <img src="/media/shroom.png" class="pixelImg">
+                    <p>You've been wandering through the woods for a long time.<br>
+                    Energy is running low, tummy's rumbling...<br>
+                    You come across a strange looking mushroom in the lands.<br>
+                    It looks delicious, but is it safe to eat something like this?
+                </p>
+                </div>
+                <div class="dualButtons">
+                    <button onclick="changeScene(6)">TAKE THE<br> CHANCE!</button>
+                    <button class ="redText" onclick="changeScene(7)">STRANGER<br>DANGER!</button>
+                </div>
+            </div>
+        `
+        ,
+        nextScenes: [6, 7]
+        
     }
     ,
     {// Scene 6 (Ate the mushroom)
@@ -121,26 +130,148 @@ const pages = [
     </div>
     </div>
     `
+    ,
+    nextScenes: [8]
     }
     ,
     {// Scene 7 (Did not eat the mushroom)
         content: /*html*/ `
-    <div class="centerContainer cardBox">   
-        <div class="card">
-        <div class="nextContainer">
-        <p>It's not worth it... <br>
-        I know there's some really messed up mushrooms, my mission is too important.<br>
-        I feel awfully hungry and slow, but hopefully there'll be a tavern of sorts ahead.<br>
-        I mean... even evil forces need to eat, right?
-        </p>  
-            <img onclick="changeScene(xxx)" class="nextArrow" src="/media/nextarrow.png"> 
+            <div class="centerContainer cardBox">   
+                <div class="card">
+                    <div class="nextContainer">
+                        <p>It's not worth it... <br>
+                        I know there's some really messed up mushrooms, my mission is too important.<br>
+                        I feel awfully hungry and slow, but hopefully there'll be a tavern of sorts ahead.<br>
+                        I mean... even evil forces need to eat, right?
+                        </p>  
+                        <img onclick="changeScene(9)" class="nextArrow" src="/media/nextarrow.png"> 
+                    </div>
+                </div>
             </div>
-    </div>
-    </div>
+    `
+    ,
+    nextScenes: [9]
+    }
+    ,
+    {// Scene 8 (Ate the mushroom -> Enter fight)
+        content: function () {
+            return /*html*/ `
+            <div class="centerContainer cardBox">   
+                <div class="cardColumn">
+                    <div class="nextContainer">
+                        <h3>Strange encounter!</h3>
+                        <p class="redText">"WHO ARE YOU?"</p>
+                        <p class="smallText"> - says a funny looking creature in a silly tone.</p>
+                        <p>Oh, my name is ${name}, I'm here to defeat all evil!</p><br>
+                        <p class="smallText">(When I look at this creature I'm withholding the urge to laugh. He's got 4 eyes, a pink glowing aura and a hole in their stomache for ice cream.<br>
+                         NO WAY he can be dangerous!)</p>
+                        <p class="redText">"Defeat all evil huh? *laugh*<br> I would never let such a small critter like you defeat me!<br>
+                        Are you gonna defend your honour with that plastic sword of yours? *laugh*"</p>             
+                    </div>
+                </div>
+                    <div class="dualButtons">
+                        <button onclick="changeScene(10)">COME AT ME!</button>
+                    <button class ="redText" onclick="changeScene(11)">ESCAPE</button>
+                    </div>
+            </div>
     `
     }
+    ,
+    nextScenes: [10]
+    }
+    ,
+    {// Scene 9 (Did not eat the mushroom -> Enter fight)
+        content: function () {
+            return /*html*/ `
+            <div class="centerContainer cardBox">   
+                <div class="cardColumn">
+                    <div class="nextContainer">
+                        <h3>Strange encounter!</h3>
+                        <p class="redText">"WHO ARE YOU?"</p>
+                        <p class="smallText smallText">- Says a stranger with a strong tone.</p>
+                        <p>Oh, my name is ${name}, I'm here to defeat all evil!</p>
+                        <p class="smallText redText">"Defeat all evil huh?"<br>
+                        This land has been free from any evil forces for hundreds of years. Who told you this?</p>             
+                        <p>Actually, coming to think of it, I have no idea...</p>
+                        <p>I just ran across this weirdo in the vast area of Woodlands who told me I should take upon this quest.</p>
+                        <p class="smallText redText">"Oh no...<br>
+                        You must've met Jake. He's a crazy head eating lot's of poisonous mushrooms, he rambles on about quests and evil.</p>
+                        <p class="smallText">I've seen those... Orange with pink spots... Close call...</p>
+                    </div>
+                    <img onclick="changeScene(12)" class="nextArrow" src="/media/nextarrow.png"> 
+                </div>
+            </div>
+    `
+    }
+    ,
+    nextScenes: [12]
+    }
+    ,
+    {// Scene 10 (Pressed "Come At me!")
+        content: function () {
+            return /*html*/ `
+            <div class="centerContainer cardBox">   
+                <div class="cardColumn">
+                    <div class="nextContainer">
+                       <p class="smallText"> ${name} wakes up with a terrible pain in the back of the head.</p>
+                        <p>"What? Where am I...? What theee?"<br><br>
+                        Looking around ${name} notice nothing but woods, all regular boring woods.<br><br>
+                        Was this all a dream?<br><br>
+                        "Why am I covered in ice cream?"<br><br>
+                    <h3 class="blink"> To be continued...</h3>
+                    <button class ="redText" onclick="changeScene(0)">EXIT</button>
+                </div>
+            </div>
+    `
+    }
+    }
+    ,
+    {// Scene 11 (Pressed "ESCAPE")
+        content: function () {
+            return /*html*/ `
+            <div class="centerContainer cardBox">   
+                <div class="cardColumn">
+                    <div class="nextContainer">
+                       <p class="smallText"> You try to run, but the nasty creature is following you.<br><br>
+                       The whole world has taken a turn to worse, it's all covered in blood with dangers lurking behind every stone.<br><br>
+                       You see a bright light, run towards it, the second you're about to enter there's a loud...<br>
+                       <p class ="redText">BANG!</p>
+                       <p>Silence...<br><br>
+                        You can't see anything, feel anything.<br><br>
+                         It's all... gone.</p><br>
+                    <button class ="redText" onclick="changeScene(DEAD)">EXIT</button>
 
+                </div>
+            </div>
+    `
+    }
+    },
+    {// Scene 12 (Did not eat the mushroom -> Had encounter -> Next scene)
+        content: function () {
+            return /*html*/ `
+            <div class="centerContainer cardBox">   
+                <div class="cardColumn">
+                    <div class="nextContainer">
+                    <p class="smallText redText">"You look a bit tired."</p>
+                    <p class="smallText">"Yeah, long night. I'm starving actually"</p>
+                    <p class="smallText redText">"I know what you need, follow me, I don't live to far off from here."</p>
+                    <p class="smallText"> The two strangers wander off through the forest until a small cabin appears. They enter and in the kitchen there's a big pot cooking some kind of stew.</p>
+                    <p class="smallText redText">"Take a seat, I'll get you some food, heroes need energy to stay strong."</p>
+                    <p class="smallText"> And there they sat, enjoying their meal, when ${name} noticed something odd.<br>
+                    Pink spots in the stew. Many of them.</p><br>
+                    <h3 class="blink"> To be continued...</h3>
+                    
+                </div>
+                <button class ="redText" onclick="changeScene(0)">EXIT</button>
+                </div>
+            </div>
+    `
+    }
+}   
 ]
+
+
+
 
 changeScene = function (nextPage, errorText) {
     activePage = nextPage
